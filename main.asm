2bff8db01a5d0ec0b020fba12debe9a71f998ed6
Synrone <vakulasan@gmail.com>
2021-07-25 18:36:53 +0300

    25.07.2021

--- main.asm
+++ main.asm
@@ -4,15 +4,7 @@
 .include "include.inc"
 
 init:     Initialization
-          
-lp:       cbi     PORTB, SS
-          ldi     temp,   0x55
-          out     SPDR,   temp
-          rcall wait_SPI_free
-          ldi     temp,   0x33
-          out     SPDR,   temp
-          rcall wait_SPI_free 
-          sbi     PORTB, SS 
+
 loop:     rjmp loop
 
 
