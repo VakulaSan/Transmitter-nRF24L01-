33c5ce2f3eec03a68eed30fd149a53e39b7e803e
Synrone <vakulasan@gmail.com>
2021-07-25 16:44:18 +0300

    25/07/2021

--- main.asm
+++ main.asm
@@ -1,8 +1,16 @@
 .device  ATmega328p
 .include "macro.inc"
+.include "nRF24L01+.inc"
 .include "include.inc"
 
-
+init:     Initialization
+          cbi     PORTB, SS
+          ldi     temp,   (R_REGISTER<<4) | (1<<CONFIG)
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          ldi     temp,   (1<<PWR_UP)
+          rcall wait_SPI_free  
+loop:     rjmp init
 
 
           
