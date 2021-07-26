320e6492fce30130a8737b06f06699c1826a64ae
Synrone <vakulasan@gmail.com>
2021-07-26 14:51:34 +0300

    Merge

--- main.asm
+++ main.asm
@@ -6,24 +6,33 @@
 init:     Initialization
           ldi     temp,   0xFF
           out     DDRC,   temp
+
+          rcall delay
           
-lp:       cbi     PORTB, SS
-          ldi     temp,   0x33
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (CONFIG)
           out     SPDR,   temp
-          
           rcall wait_SPI_free
           sbi     PORTC, 3
-
-          ldi     temp,   0x33
+          ldi     temp,   (1<<PWR_UP) 
           out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
+          cbi     PORTC, 3
           
+          cbi     PORTB, SS
+          ldi     temp,   W_TX_PAYLOAD
+          out     SPDR,   temp
           rcall wait_SPI_free
-          
-          
+          sbi     PORTC, 3
+          ldi     temp,   0xFF 
+          out     SPDR,   temp
+          rcall wait_SPI_free         
           sbi     PORTB, SS 
           sbi     PORTC, 3
-          
-loop:     rjmp lp
+          sbi     PORTB,  CE
+          rcall   delay  
+loop:     rjmp loop
 
 
           
