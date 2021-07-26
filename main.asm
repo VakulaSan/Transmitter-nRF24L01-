ba3382e4b095ec1ab46a3fb5f687dc17a9def824
Syncrone <vakulasan@gmail.com>
2021-07-27 01:42:57 +0300

    26.07.2021

--- main.asm
+++ main.asm
@@ -4,7 +4,7 @@
 .include "include.inc"
 
 init:     Initialization
-          ldi     temp,   0xFF
+lp:       ldi     temp,   0xFF
           out     DDRC,   temp
 
           
@@ -13,26 +13,39 @@ init:     Initialization
           ldi     temp,   (W_REGISTER) | (CONFIG)
           out     SPDR,   temp
           rcall wait_SPI_free
-          sbi     PORTC, 3
+          
           ldi     temp,   (1<<PWR_UP) 
           out     SPDR,   temp
           rcall wait_SPI_free         
           sbi     PORTB, SS 
-          cbi     PORTC, 3
+          
           
           cbi     PORTB, SS
           ldi     temp,   W_TX_PAYLOAD
           out     SPDR,   temp
           rcall wait_SPI_free
-          sbi     PORTC, 3
-          ldi     temp,   0xFF 
+          
+          ldi     temp,   0x55
           out     SPDR,   temp
           rcall wait_SPI_free         
           sbi     PORTB, SS 
-          sbi     PORTC, 3
-          sbi     PORTB,  CE
+          
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (RX_PW_P0)
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          
+          ldi     temp,   0x01   
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
+
            
-loop:     rjmp loop
+          sbi     PORTB,  CE
+          
+           
+loop:     rjmp lp
 
 
           
