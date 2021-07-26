59568fde7446bcdd401430882aee689c431ddfd1
Syncrone <vakulasan@gmail.com>
2021-07-27 01:59:10 +0300

    dd

--- main.asm
+++ main.asm
@@ -12,37 +12,34 @@ lp:       ldi     temp,   0xFF
           cbi     PORTB, SS
           ldi     temp,   (W_REGISTER) | (CONFIG)
           out     SPDR,   temp
-          rcall wait_SPI_free
-          
-          ldi     temp,   (1<<PWR_UP) 
+          rcall wait_SPI_free          
+          ldi     temp,   (1<<PWR_UP) | (1<<PRIM_RX)
           out     SPDR,   temp
           rcall wait_SPI_free         
-          sbi     PORTB, SS 
-          
-          
-          cbi     PORTB, SS
-          ldi     temp,   W_TX_PAYLOAD
-          out     SPDR,   temp
-          rcall wait_SPI_free
-          
-          ldi     temp,   0x55
-          out     SPDR,   temp
-          rcall wait_SPI_free         
-          sbi     PORTB, SS 
-          
+          sbi     PORTB, SS      
 
           cbi     PORTB, SS
           ldi     temp,   (W_REGISTER) | (RX_PW_P0)
           out     SPDR,   temp
-          rcall wait_SPI_free
-          
+          rcall wait_SPI_free          
           ldi     temp,   0x01   
           out     SPDR,   temp
           rcall wait_SPI_free         
           sbi     PORTB, SS 
 
+          
            
           sbi     PORTB,  CE
+          rcall delay
+          
+          cbi     PORTB, SS
+          ldi     temp,   R_RX_PAYLOAD
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   NOP_
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
           
            
 loop:     rjmp lp
