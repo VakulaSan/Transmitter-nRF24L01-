80c075d1683bc7ea52b4286f7e3e55904be658fa
Synrone <vakulasan@gmail.com>
2021-07-28 14:49:43 +0300

    1 TRY TO TRANSMIT

--- main.asm
+++ main.asm
@@ -4,47 +4,135 @@
 .include "include.inc"
 
 init:     Initialization
-lp:       ldi     temp,   0xFF
+          ldi     temp,   0xFF
           out     DDRC,   temp
 
-          
+          rcall delay          
           
           cbi     PORTB, SS
           ldi     temp,   (W_REGISTER) | (CONFIG)
           out     SPDR,   temp
-          rcall wait_SPI_free
-          
-          ldi     temp,   (1<<PWR_UP) 
+          rcall wait_SPI_free          
+          ldi     temp,   (1<<PWR_UP) | (0<<PRIM_RX) | (1<<EN_CRC) 
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS      
+
+          rcall delay 
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (EN_AA)
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   (1<<ENAA_P0)   
           out     SPDR,   temp
           rcall wait_SPI_free         
           sbi     PORTB, SS 
-          
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (EN_RXADDR)
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   (1<<ERX_P0)   
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (SETUP_AW)
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   (SETUP_AW_5BYTES_ADDRESS)   
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
           
           cbi     PORTB, SS
-          ldi     temp,   W_TX_PAYLOAD
+          ldi     temp,   (W_REGISTER) | (RF_CH)
           out     SPDR,   temp
-          rcall wait_SPI_free
-          
-          ldi     temp,   0x55
+          rcall wait_SPI_free          
+          ldi     temp,   (0x02)   
           out     SPDR,   temp
           rcall wait_SPI_free         
           sbi     PORTB, SS 
-          
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (RF_SETUP)
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   (0x11<<RF_PWR) | (0<<RF_DR_LOW) | (1<<RF_DR_HIGH)   
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (STATUS)
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   (1<<RX_DR) | (0<<TX_DS) | (1<<MAX_RT)   
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (RX_ADDR_P0)
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free  
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          sbi     PORTB, SS 
+
+          cbi     PORTB, SS
+          ldi     temp,   (W_REGISTER) | (TX_ADDR)
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free  
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          ldi     temp,   0xE7   
+          out     SPDR,   temp
+          rcall wait_SPI_free
+          sbi     PORTB, SS 
+
 
           cbi     PORTB, SS
           ldi     temp,   (W_REGISTER) | (RX_PW_P0)
           out     SPDR,   temp
-          rcall wait_SPI_free
-          
+          rcall wait_SPI_free          
           ldi     temp,   0x01   
           out     SPDR,   temp
           rcall wait_SPI_free         
-          sbi     PORTB, SS 
+          sbi     PORTB, SS
+    
+lp:       sbi     PORTB,  CE
+          rcall delay
+          cbi     PORTB, CE
 
-
-          sbi     PORTB,  CE
+                   
            
-loop:     rjmp loop
+loop:     rjmp lp
 
 
           
