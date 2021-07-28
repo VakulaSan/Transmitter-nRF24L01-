e4051a0df6b2b34fad7de50b00555e5344273cb8
Synrone <vakulasan@gmail.com>
2021-07-28 14:49:43 +0300

    1 TRY TO TRANSMIT

--- main.asm
+++ main.asm
@@ -4,47 +4,76 @@
 .include "include.inc"
 
 init:     Initialization
-lp:       ldi     temp,   0xFF
-          out     DDRC,   temp
+          rcall delay
 
-          
+          cbi     PORTB, SS
+          ldi     temp,   (FLUSH_TX)
+          out     SPDR,   temp
+          rcall wait_SPI_free     
+          sbi     PORTB, SS 
           
           cbi     PORTB, SS
           ldi     temp,   (W_REGISTER) | (CONFIG)
           out     SPDR,   temp
-          rcall wait_SPI_free
-          
+          rcall wait_SPI_free          
           ldi     temp,   (1<<PWR_UP) 
           out     SPDR,   temp
           rcall wait_SPI_free         
           sbi     PORTB, SS 
           
-          
-          cbi     PORTB, SS
-          ldi     temp,   W_TX_PAYLOAD
-          out     SPDR,   temp
-          rcall wait_SPI_free
-          
-          ldi     temp,   0x55
-          out     SPDR,   temp
-          rcall wait_SPI_free         
-          sbi     PORTB, SS 
-          
+          rcall delay
 
           cbi     PORTB, SS
           ldi     temp,   (W_REGISTER) | (RX_PW_P0)
           out     SPDR,   temp
-          rcall wait_SPI_free
-          
+          rcall wait_SPI_free          
           ldi     temp,   0x01   
           out     SPDR,   temp
           rcall wait_SPI_free         
           sbi     PORTB, SS 
 
+lp:       cbi     PORTB, SS
+          ldi     temp,   W_TX_PAYLOAD
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   0x66
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
+
+          cbi     PORTB, SS
+          ldi     temp,   W_TX_PAYLOAD
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   0x77
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
+
+          cbi     PORTB, SS
+          ldi     temp,   W_TX_PAYLOAD
+          out     SPDR,   temp
+          rcall wait_SPI_free          
+          ldi     temp,   0x88
+          out     SPDR,   temp
+          rcall wait_SPI_free         
+          sbi     PORTB, SS 
+          
+          cbi     PORTB, SS
+          ldi     temp,   (FIFO_STATUS)
+          out     SPDR,   temp
+          rcall wait_SPI_free 
+          ldi     temp,   NOP_
+          out     SPDR,   temp
+          rcall wait_SPI_free      
+          sbi     PORTB, SS           
+
+          
 
           sbi     PORTB,  CE
-           
-loop:     rjmp loop
+          rcall delay 
+          cbi     PORTB,  CE 
+loop:     rjmp lp
 
 
           
